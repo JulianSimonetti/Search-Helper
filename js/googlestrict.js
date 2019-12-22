cd6a9224573caf993fba165725f389d0974c9c65
Julian Simonetti <guerrier10@live.fr>
2019-12-22 17:01:01 +0100

    Ajout recherche site

--- js/googlestrict.js
+++ js/googlestrict.js
@@ -22,17 +22,34 @@ function validateActionRelative() {
     return;
 }
 
+function validateActionSite() {
+    document.getElementById("site").addEventListener('keydown', (e) => {
+        if (e.key === 'Enter' && hasClicked == false) {
+            goSearchStrict();
+            hasClicked = true;
+        }
+    });
+
+    return;
+}
+
 function goSearchStrict() {
     const strict = document.getElementById("strict").value;
     const relative = document.getElementById("relative").value;
+    const site = document.getElementById("site").value;
     var query = "";
+    var uQuery = "";
 
     var sTerms = strict.split(/(?:,+[ ]*)+/g);
     var sQuery = "";
     sTerms.forEach(term => {
         sQuery += '\"' + term + '\"' + ' ';
     });
-    query = (sQuery + relative).trim();
+
+    if (site != "") {
+        uQuery = "site:" + site;
+    }
+    query = ((sQuery + relative).trim() + ' ' + uQuery).trim();
  
     var url = "https://google.com#q=" + query;
     window.open(url);

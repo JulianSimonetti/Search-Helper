c1139cd945e4fd8b5a2797cc3e1357fd1dd057c2
Julian Simonetti <guerrier10@live.fr>
2019-12-23 12:45:35 +0100

    Implemented term exclusion

--- js/googlestrict.js
+++ js/googlestrict.js
@@ -1,7 +1,8 @@
 var hasClicked = false;
 
-function validateActionStrict() {
-    document.getElementById("strict").addEventListener('keydown', (e) => {
+//function validateActionStrict() {
+function validateActionStrict(field) {
+    document.getElementById(field).addEventListener('keydown', (e) => {
         if (e.key === 'Enter' && hasClicked == false) {
             goSearchStrict();
             hasClicked = true;
@@ -11,47 +12,62 @@ function validateActionStrict() {
     return;
 }
 
-function validateActionRelative() {
-    document.getElementById("relative").addEventListener('keydown', (e) => {
-        if (e.key === 'Enter' && hasClicked == false) {
-            goSearchStrict();
-            hasClicked = true;
-        }
-    });
+//function validateActionRelative() {
+//    document.getElementById("relative").addEventListener('keydown', (e) => {
+//        if (e.key === 'Enter' && hasClicked == false) {
+//            goSearchStrict();
+//            hasClicked = true;
+//        }
+//    });
 
-    return;
-}
+//    return;
+//}
 
-function validateActionSite() {
-    document.getElementById("site").addEventListener('keydown', (e) => {
-        if (e.key === 'Enter' && hasClicked == false) {
-            goSearchStrict();
-            hasClicked = true;
-        }
-    });
+//function validateActionSite() {
+//    document.getElementById("site").addEventListener('keydown', (e) => {
+//        if (e.key === 'Enter' && hasClicked == false) {
+//            goSearchStrict();
+//            hasClicked = true;
+//        }
+//    });
 
-    return;
-}
+//    return;
+//}
 
 function goSearchStrict() {
     const strict = document.getElementById("strict").value;
+    const excluded = document.getElementById("excluded").value;
     const relative = document.getElementById("relative").value;
     const site = document.getElementById("site").value;
+    if ([strict, excluded, relative, site].some(valeur => valeur != "") == false) {return;}
     var query = "";
     var uQuery = "";
 
-    var sTerms = strict.split(/(?:,+[ ]*)+/g);
+    var sTerms = strict.substring(strict.indexOf(/[^, ]+/g) + 1).split(/(?:,+[ ]*)+/g);
     var sQuery = "";
-    sTerms.forEach(term => {
-        sQuery += '\"' + term + '\"' + ' ';
+    sTerms.forEach(sTerm => {
+        sQuery += '\"' + sTerm + '\"' + ' ';
     });
 
-    if (site != "") {
-        uQuery = "site:" + site;
+    var eTerms = excluded.substring(excluded.indexOf(/[^, ]+/g) + 1).split(/(?: +|[,]+)+/g);
+    var eQuery = "";
+    if (eTerms.some(e => (/.+/g).test(e)) == true) {
+        eTerms.forEach(eTerm => {
+            eQuery += '-' + eTerm + ' ';
+        });
     }
+
+    if (eQuery != "") {
+        uQuery += eQuery;
+    }
+
+    if (site != "") {
+        uQuery += "site:" + site;
+    }
+
     query = ((sQuery + relative).trim() + ' ' + uQuery).trim();
  
-    var url = "https://google.com#q=" + query;
+    var url = "https://google.com#q=" + query.replace("\"\"", "");
     window.open(url);
     return;
 }
